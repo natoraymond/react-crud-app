903b257c964b7ab8b1405b747f9f5745306a62fc
Natoraymod <nnamdi.raymondoct@gmail.com>
2023-07-11 06:30:25 -0700

    first commit

---
+++ src/components/AddUser.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import axios from "axios";
+import {Container, Row, Col} from 'react-bootstrap'
+import { useNavigate } from "react-router-dom";
+import '../App.css'
+ 
+const AddUser = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("select");
+  const navigate = useNavigate();
+ 
+  const saveUser = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5000/users", {
+        name,
+        email,
+        gender,
+      });
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+ 
+  return (
+    <Container>
+      <Row>
+        <Col md={4}></Col>
+        <Col md={4}>   
+          <form onSubmit={saveUser}>
+            <div className="field">
+              <label className="label">Name</label>
+              <div className="control">
+                <input
+                  type="text"
+                  className="input"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="Name"
+                />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Email</label>
+              <div className="control">
+                <input
+                  type="email"
+                  className="input"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Email"
+                />
+              </div>  
+            </div>
+            <div className="field">
+              <label className="label">Gender</label>
+              <div className="control">
+                <div className="select is-fullwidth">
+                  <select
+                    value={gender}
+                    onChange={(e) => setGender(e.target.value)}
+                  >
+                   <option value="select">--- Gender ---</option> 
+                   <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div className="field">
+              <div className="control">
+                <button type="submit" className="btn btn-success save-btn">
+                  Save
+                </button>
+              </div>
+            </div>
+          </form>      
+        </Col>
+        <Col md={4}></Col>
+      </Row>
+    </Container>
+  );
+};
+ 
+export default AddUser;
---
+++ src/components/UserList.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import {Container, Row, Col} from 'react-bootstrap'
+import '../App.css'
+ 
+const UserList = () => {
+  const [users, setUser] = useState([]);
+ 
+  useEffect(() => {
+    getUsers();
+  }, []);
+ 
+  const getUsers = async () => {
+    const response = await axios.get("http://localhost:5000/users");
+    setUser(response.data);
+  };
+ 
+  const deleteUser = async (id) => {
+    try {
+      await axios.delete(`http://localhost:5000/users/${id}`);
+      getUsers();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+ 
+  return (
+    <Container>
+      <Row>
+        <Col md={2}></Col>
+
+        <Col md={8}>
+          <Link to="add" className="btn btn-success">
+            Add New Student
+          </Link><br/><br/>
+          <table class="table table-responsive">        
+            <thead className="thead-light">
+              <tr>
+                <th scope="col">No</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Gender</th>
+                <th scope="col" colSpan="2" className="text-center">Actions</th>
+              </tr>
+            </thead>
+            
+            <tbody>
+              {users.map((user, index) => (
+                <tr key={user._id}>
+                  <td>{index + 1}</td>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.gender}</td>
+                  <td>
+                    <Link
+                      to={`edit/${user._id}`}
+                      className="btn btn-info is-small mr-1"
+                    >
+                      Edit
+                    </Link>
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => deleteUser(user._id)}
+                      className="btn btn-danger is-small mr-1"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>          
+        </Col>
+
+        <Col md={2}></Col>      
+      </Row>    
+  </Container>
+  );
+};
+ 
+export default UserList;
